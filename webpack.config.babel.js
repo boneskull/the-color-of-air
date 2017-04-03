6f96c44afab98fcd19cf7d34cd240202a4e11332
Christopher Hiller <boneskull@boneskull.com>
2017-04-02 18:34:21 -0700

    actually use babel-loader; provide minified output; provide UMD bundle

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,12 +1,37 @@
 import {join} from 'path';
+import Babili from 'babili-webpack-plugin';
+import webpackMerge from 'webpack-merge';
 
-export default {
+const baseConfig = {
   entry: join(__dirname, 'src', 'index.js'),
   output: {
     path: join(__dirname, 'dist'),
-    filename: 'the-color-of-air.js',
-    libraryTarget: 'commonjs2',
+    libraryTarget: 'umd',
     library: 'the-color-of-air'
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        include: join(__dirname, 'src'),
+        loader: 'babel-loader'
+      }
+    ]
+  }
 };
+
+export default [
+  webpackMerge(baseConfig, {
+    output: {
+      filename: 'the-color-of-air.js'
+    }
+  }),
+  webpackMerge(baseConfig, {
+    output: {
+      filename: 'the-color-of-air.min.js'
+    },
+    plugins: [
+      new Babili()
+    ]
+  })
+];
